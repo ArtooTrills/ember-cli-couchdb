17d7a762818f7e19002f3edbd49bb497b2fa3f5f
Dilip <dilip@artoo.in>
2016-01-28 19:50:02 +0530

    Update 2016-01-28T14:19:56.633Z

---
+++ api.js
@@ -0,0 +1,23 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ember-cli-couchdb.DataAdapter"
+    ],
+    "modules": [
+        "Adapter",
+        "Home"
+    ],
+    "allModules": [
+        {
+            "displayName": "Adapter",
+            "name": "Adapter"
+        },
+        {
+            "displayName": "Home",
+            "name": "Home",
+            "description": "`ember-cli-couchdb` provides two adpaters for store to connect with couchdb\n- DataAdapter\n\t\tdata adapter is to fetch and store the couchdb documents\n\t\t\n- ViewAdapter\n\t\tview adapter is to get data from couchdb design docs"
+        }
+    ],
+    "elements": []
+} };
+});
